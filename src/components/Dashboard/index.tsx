d32fcad6e30670ac90a93d6ecd13d4d75a000bcb
Rafael Eduardo Kepler <55624017+RafaelEdKepler@users.noreply.github.com>
2021-07-13 20:22:37 -0300

    Aula Componente: TransactionsTable - Finalizada

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./style";
 import { Summary } from "../Summary";
+import { TranscationTable } from "../TransactionsTable";
 
 export function Dashboard() {
     return (
         <Container>
             <Summary/>
+            <TranscationTable/>
         </Container>
     );
 }

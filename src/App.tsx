12a676adb73c519077dbc3b2e482802dcd53cd7b
Rafael Eduardo Kepler <55624017+RafaelEdKepler@users.noreply.github.com>
2021-07-16 11:04:14 -0300

    Aula Componente: NewTransactionModel - Finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Header } from "./components/Header";
 import Modal from 'react-modal';
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
-Modal.setAppElement('root');
+Modal.setAppElement('#root');
 
 export function App() {
 
@@ -21,12 +22,7 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
-      <Modal 
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-      >
-          <h2>Cadastrar transação</h2>
-      </Modal>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <Dashboard/>
       <GlobalStyle/>
     </>

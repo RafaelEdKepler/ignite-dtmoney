82f63785bbbc01b29968e9f86d8e7d7a5884931f
Rafael Eduardo Kepler <55624017+RafaelEdKepler@users.noreply.github.com>
2021-07-21 18:11:58 -0300

    Aula Formatando Valores - Finalizada

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -37,9 +37,16 @@ export function TranscationTable() {
                     {transactions.map(item => (
                         <tr key={item.id}>
                             <td>{item.title}</td>
-                            <td className={item.type}>{item.amount}</td>
+                            <td className={item.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(item.amount)}
+                            </td>
                             <td>{item.category}</td>
-                            <td>{item.createdAt}</td>
+                            <td>{new Intl.DateTimeFormat('pt-BR').format(
+                                new Date(item.createdAt)
+                            )}</td>
                         </tr>
                     ))}
                  </tbody>
